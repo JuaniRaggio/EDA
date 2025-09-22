cc8d678843e19631e6ecddab7edc44545831a914
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-09-22 19:39:18 -0300

    parciales viejos

---
+++ parciales_viejos/src/main/java/com/primeros_parciales/App.java
@@ -0,0 +1,13 @@
+package com.primeros_parciales;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
